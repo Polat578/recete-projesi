718d04f72ece1ff02cc88507e443af5b06a361c6
Polat578 <toyzzonesatis@gmail.com>
2025-07-24 15:38:59 +0300

    App.py

--- app.py
+++ app.py
@@ -1,18 +1,14 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_folder='static')
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html")  # Ana panel
 
 @app.route('/warehouses')
 def warehouses():
-    return render_template("warehouses.html")
-
-@app.route('/materials')
-def materials():
-    return render_template("materials.html")
+    return render_template("warehouses.html")  # Ambar sayfası
 
 if __name__ == '__main__':
     app.run(debug=True)
