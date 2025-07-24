716aafe015e74f0653c5b2f21d61925e08bbd1de
Polat578 <toyzzonesatis@gmail.com>
2025-07-24 16:08:55 +0300

    app.py

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='templates', static_folder='static')
+app = Flask(__name__, static_url_path='/static', static_folder='static', template_folder='templates')
 
+# Ana Sayfa
 @app.route('/')
 def index():
-    return render_template("index.html")  # Ana panel
+    return render_template("index.html")
 
+# Ambar Sayfası
 @app.route('/warehouses')
 def warehouses():
-    return render_template("warehouses.html")  # Ambar sayfası
+    return render_template("warehouses.html")
+
+# Malzeme Sayfası (Firebase ile çalışır)
+@app.route('/materials')
+def materials():
+    return render_template("materials.html")
+
+# Eğer başka sayfalar da varsa burada eklenebilir
 
 if __name__ == '__main__':
     app.run(debug=True)
